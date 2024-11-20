9c4998885e9b605a652f03e87965e069d000a762
Adnan Ahmed <adnan.ahmed0061@gmail.com>
2024-11-21 00:35:01 +0600

    test removed

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,13 +2,17 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "event_reservation",
-    });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    const conn = await mongoose.connect(
+      "mongodb+srv://adnanahmed0061:XXFHtD4pdlkHNHnl@cluster0.9bzau.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+      {
+        dbName: "event_reservation",
+      }
+    );
+    // console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
-    process.exit(1);
+    // process.exit(1);
+    throw error;
   }
 };
 
